8f463405ca913cb35b41bc80568cbd23d271b757
DoQuocBinh <binhdq@fpt.edu.vn>
2020-12-04 14:38:02 +0700

    hbs Partial

---
+++ HBSPartial/server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var app= express()
+
+
+//const engines = require('consolidate');
+//app.engine('hbs',engines.handlebars);
+//app.set('views','./views');
+var hbs = require('hbs')
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname +'/views/partials')
+
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to our Home',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt',(msg)=>{
+    return msg.toUpperCase();
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.debug('Server is runing..' + PORT);
